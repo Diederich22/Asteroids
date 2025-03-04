c6c507695f6b4e968648f4c7b529fff830cce95f
lex <lex@DESKTOP-CRLPMVP.>
2025-03-04 11:57:22 +0100

    "setting framerate"

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 
 def main():
+
+    clock = pygame.time.Clock()
+
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,12 +19,13 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return 
 
         screen.fill("black")
 
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
