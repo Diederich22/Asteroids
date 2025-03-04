fe11d9f2e0e81b646278ce8a9fa91b38d77298ce
lex <lex@DESKTOP-CRLPMVP.>
2025-03-04 13:56:22 +0100

    "turning with radius"

--- main.py
+++ main.py
@@ -21,17 +21,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
 
         screen.fill("black")
 
+        player.update(dt)
+
         player.draw(screen)
 
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
